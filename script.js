145b7b99fcdb6d9f39efcf8813cbe8f3f770d5fe
Ewerton Nascimento <epnascimento.dev@gmail.com>
2022-03-12 11:51:05 -0300

    button-clear javascript

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-const displayButtons = document.querySelectorAll('.button-bt');
-const displayOperadores = document.querySelectorAll('.operadores');
-const displayLimpar = document.querySelector('.limpar');
-const displayResultado = document.querySelector('.igual');
-const displayApagar = document.querySelector('.apagar');
-const textDisplayPrincipal = document.querySelector('.display1');
-const textDisplaySecundario = document.querySelector('.display2');
+const displayButtons = document.querySelectorAll("[button-bt]");
+const displayOperation = document.querySelectorAll("[operation-bt]");
+const displayClear = document.querySelector("[clear-bt]");
+const displayResult = document.querySelector("[result-bt]");
+const displayDelete = document.querySelector("[delete]");
+const textDisplayPrimary = document.querySelector("[display1]");
+const textDisplaySecundary = document.querySelector("[display2]");
 
+class Calculator {
+    constructor(textDisplayPrimary, textDisplaySecundary){
+        this.textDisplayPrimary = textDisplayPrimary;
+        this.textDisplaySecundary = textDisplaySecundary;
+    }
+
+    clear() {
+        this.newTextDisplayPrimary = "";
+        this.newTextDisplaySecundary = "";
+    }
+
+    changeDisplay() {
+        this.textDisplayPrimary.innerText = this.newTextDisplayPrimary;
+        this.textDisplaySecundary.innerText = this.newTextDisplaySecundary;
+    }
+} 
+
+const calc = new Calculator(textDisplayPrimary, textDisplaySecundary);
+
+displayClear.addEventListener("click", () =>{
+    calc.clear();
+    calc.changeDisplay();
+});
